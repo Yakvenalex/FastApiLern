d89dc4caf1ea099ccb2509ab8ce181142f6080bc
Alexey Yakovenko <mr.mnogo@gmail.com>
2024-07-07 05:13:04 +0300

    обновленный код под вторую статью

--- app/main.py
+++ app/main.py
@@ -1,10 +1,52 @@
-from typing import Optional
-
-from fastapi import FastAPI
-from utils import json_to_dict_list
+from fastapi import FastAPI, Depends, HTTPException
+from utils import json_to_dict_list, add_student, upd_student, dell_student
 import os
+from enum import Enum
+from pydantic import BaseModel, EmailStr, Field, field_validator
+from datetime import date, datetime
+from typing import Optional, List, Dict, Any
+import re
 
-path_to_json = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'students.json')
+
+class Major(str, Enum):
+    informatics = "Информатика"
+    economics = "Экономика"
+    law = "Право"
+    medicine = "Медицина"
+    engineering = "Инженерия"
+    languages = "Языки"
+
+
+class SStudent(BaseModel):
+    student_id: int
+    phone_number: str = Field(..., description="Номер телефона в международном формате, начинающийся с '+'")
+    first_name: str = Field(..., min_length=1, max_length=50, description="Имя студента, от 1 до 50 символов")
+    last_name: str = Field(..., min_length=1, max_length=50,
+                           description="Фамилия студента, от 1 до 50 символов")
+    date_of_birth: date = Field(..., description="Дата рождения студента в формате ГГГГ-ММ-ДД")
+    email: EmailStr = Field(..., description="Электронная почта студента")
+    address: str = Field(..., min_length=10, max_length=200,
+                         description="Адрес студента, не более 200 символов")
+    enrollment_year: int = Field(..., ge=2002, description="Год поступления должен быть не меньше 2002")
+    major: Major = Field(..., description="Специальность студента")
+    course: int = Field(..., ge=1, le=5, description="Курс должен быть в диапазоне от 1 до 5")
+    special_notes: Optional[str] = Field(None, max_length=500,
+                                         description="Дополнительные заметки, не более 500 символов")
+
+    @field_validator("phone_number")
+    @classmethod
+    def validate_phone_number(cls, values: str) -> str:
+        if not re.match(r'^\+\d{1,15}$', values):
+            raise ValueError('Номер телефона должен начинаться с "+" и содержать от 1 до 15 цифр')
+        return values
+
+    @field_validator("date_of_birth")
+    @classmethod
+    def validate_date_of_birth(cls, values: date):
+        if values and values >= datetime.now().date():
+            raise ValueError('Дата рождения должна быть в прошлом')
+        return values
+
 
 app = FastAPI()
 
@@ -16,7 +58,7 @@ def home_page():
 
 @app.get("/students")
 def get_all_students(course: Optional[int] = None):
-    students = json_to_dict_list(path_to_json)
+    students = json_to_dict_list()
     if course is None:
         return students
     else:
@@ -27,34 +69,85 @@ def get_all_students(course: Optional[int] = None):
         return return_list
 
 
+class RBStudent:
+    def __init__(self, course: int, major: Optional[str] = None, enrollment_year: Optional[int] = 2018):
+        self.course: int = course
+        self.major: Optional[str] = major
+        self.enrollment_year: Optional[int] = enrollment_year
+
+
 @app.get("/students/{course}")
-def get_all_students_course(course: int, major: Optional[str] = None, enrollment_year: Optional[int] = 2018):
-    students = json_to_dict_list(path_to_json)
+def get_all_students_course(request_body: RBStudent = Depends()) -> List[SStudent]:
+    students = json_to_dict_list()
     filtered_students = []
     for student in students:
-        if student["course"] == course:
+        if student["course"] == request_body.course:
             filtered_students.append(student)
 
-    if major:
-        filtered_students = [student for student in filtered_students if student['major'].lower() == major.lower()]
+    if request_body.major:
+        filtered_students = [student for student in filtered_students if
+                             student['major'].lower() == request_body.major.lower()]
 
-    if enrollment_year:
-        filtered_students = [student for student in filtered_students if student['enrollment_year'] == enrollment_year]
+    if request_body.enrollment_year:
+        filtered_students = [student for student in filtered_students if
+                             student['enrollment_year'] == request_body.enrollment_year]
 
     return filtered_students
 
 
 @app.get("/student/{student_id}")
 def get_student_from_id(student_id: int):
-    students = json_to_dict_list(path_to_json)
+    students = json_to_dict_list()
     for student in students:
         if student["student_id"] == student_id:
             return student
 
 
 @app.get("/student")
-def get_student_from_param_id(student_id: int):
-    students = json_to_dict_list(path_to_json)
+def get_student_from_param_id(student_id: int) -> SStudent:
+    students = json_to_dict_list()
     for student in students:
         if student["student_id"] == student_id:
             return student
+
+
+@app.post("/add_student")
+def add_student_handler(student: SStudent):
+    check = add_student(student.dict())
+    if check:
+        return {"message": "Студент успешно добавлен!"}
+    else:
+        raise HTTPException(status_code=400, detail="Ошибка при удалении студента")
+
+# Определение модели для фильтра обновления
+class SUpdateFilter(BaseModel):
+    student_id: int
+
+
+# Определение модели для новых данных студента
+class SStudentUpdate(BaseModel):
+    course: int = Field(..., ge=1, le=5, description="Курс должен быть в диапазоне от 1 до 5")
+    major: Optional[Major] = Field(..., description="Специальность студента")
+
+
+@app.put("/update_student")
+def update_student_handler(filter_student: SUpdateFilter, new_data: SStudentUpdate):
+    check = upd_student(filter_student.dict(), new_data.dict())
+    if check:
+        return {"message": "Информация о студенте успешно обновлена!"}
+    else:
+        raise HTTPException(status_code=400, detail="Ошибка при обновлении информации о студенте")
+
+
+class SDeleteFilter(BaseModel):
+    key: str
+    value: Any
+
+
+@app.delete("/delete_student")
+def delete_student_handler(filter_student: SDeleteFilter):
+    check = dell_student(filter_student.key, filter_student.value)
+    if check:
+        return {"message": "Студент успешно удален!"}
+    else:
+        raise HTTPException(status_code=400, detail="Ошибка при удалении студента")
--- utils.py
+++ utils.py
@@ -1,36 +1,28 @@
-import json
+from json_db_lite import JSONDatabase
+
+# инициализация объекта
+small_db = JSONDatabase(file_path='students.json')
 
 
-def dict_list_to_json(dict_list, filename):
-    """
-    Преобразует список словарей в JSON-строку и сохраняет её в файл.
-
-    :param dict_list: Список словарей
-    :param filename: Имя файла для сохранения JSON-строки
-    :return: JSON-строка или None в случае ошибки
-    """
-    try:
-        json_str = json.dumps(dict_list, ensure_ascii=False)
-        with open(filename, 'w', encoding='utf-8') as file:
-            file.write(json_str)
-        return json_str
-    except (TypeError, ValueError, IOError) as e:
-        print(f"Ошибка при преобразовании списка словарей в JSON или записи в файл: {e}")
-        return None
+# получаем все записи
+def json_to_dict_list():
+    return small_db.get_all_records()
 
 
-def json_to_dict_list(filename):
-    """
-    Преобразует JSON-строку из файла в список словарей.
+# добавляем студента
+def add_student(student: dict):
+    student['date_of_birth'] = student['date_of_birth'].strftime('%Y-%m-%d')
+    small_db.add_records(student)
+    return True
 
-    :param filename: Имя файла с JSON-строкой
-    :return: Список словарей или None в случае ошибки
-    """
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            json_str = file.read()
-            dict_list = json.loads(json_str)
-        return dict_list
-    except (TypeError, ValueError, IOError) as e:
-        print(f"Ошибка при чтении JSON из файла или преобразовании в список словарей: {e}")
-        return None
+
+# обновляем данные по студенту
+def upd_student(upd_filter: dict, new_data: dict):
+    small_db.update_record_by_key(upd_filter, new_data)
+    return True
+
+
+# удаляем студента
+def dell_student(key: str, value: str):
+    small_db.delete_record_by_key(key, value)
+    return True
