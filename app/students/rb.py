388912f6ac93ea1a3502b9efe50e57ba61072369
Yakovenko Oleksii <mr.mnogo@gmail.com>
2024-07-09 15:36:52 +0300

    upd

--- app/students/rb.py
+++ app/students/rb.py
@@ -1,8 +1,17 @@
-from typing import Optional
-
-
 class RBStudent:
-    def __init__(self, course: int, major: Optional[str] = None, enrollment_year: Optional[int] = 2018):
-        self.course: int = course
-        self.major: Optional[str] = major
-        self.enrollment_year: Optional[int] = enrollment_year
+    def __init__(self, student_id: int | None = None,
+                 course: int | None = None,
+                 major_id: int | None = None,
+                 enrollment_year: int | None = None):
+        self.id = student_id
+        self.course = course
+        self.major_id = major_id
+        self.enrollment_year = enrollment_year
+
+    def to_dict(self) -> dict:
+        return {key: value for key, value in {
+            'id': self.id,
+            'course': self.course,
+            'major_id': self.major_id,
+            'enrollment_year': self.enrollment_year
+        }.items() if value is not None}
--- app/students/schemas.py
+++ app/students/schemas.py
@@ -1,23 +1,13 @@
 from datetime import datetime, date
-from enum import Enum
-from typing import Any, Optional
+from typing import Optional
 import re
 
 from pydantic import BaseModel, Field, field_validator, EmailStr, ConfigDict
 
 
-class SMajor(str, Enum, ):
-    informatics = "Информатика"
-    economics = "Экономика"
-    law = "Право"
-    medicine = "Медицина"
-    engineering = "Инженерия"
-    languages = "Языки"
-
-
 class SStudent(BaseModel):
     model_config = ConfigDict(from_attributes=True)
-    student_id: int
+    id: int
     phone_number: str = Field(..., description="Номер телефона в международном формате, начинающийся с '+'")
     first_name: str = Field(..., min_length=1, max_length=50, description="Имя студента, от 1 до 50 символов")
     last_name: str = Field(..., min_length=1, max_length=50,
@@ -27,7 +17,7 @@ class SStudent(BaseModel):
     address: str = Field(..., min_length=10, max_length=200,
                          description="Адрес студента, не более 200 символов")
     enrollment_year: int = Field(..., ge=2002, description="Год поступления должен быть не меньше 2002")
-    major_id: int = Field(..., description="ID специальности студента")
+    major_id: int = Field(..., ge=1, description="ID специальности студента")
     course: int = Field(..., ge=1, le=5, description="Курс должен быть в диапазоне от 1 до 5")
     special_notes: Optional[str] = Field(None, max_length=500,
                                          description="Дополнительные заметки, не более 500 символов")
@@ -45,22 +35,3 @@ class SStudent(BaseModel):
         if values and values >= datetime.now().date():
             raise ValueError('Дата рождения должна быть в прошлом')
         return values
-
-
-# Определение модели для фильтра обновления
-class SUpdateFilter(BaseModel):
-    student_id: int
-    model_config = ConfigDict(from_attributes=True)
-
-
-# Определение модели для новых данных студента
-class SStudentUpdate(BaseModel):
-    course: int = Field(..., ge=1, le=5, description="Курс должен быть в диапазоне от 1 до 5")
-    major: Optional[Major] = Field(..., description="Специальность студента")
-    model_config = ConfigDict(from_attributes=True)
-
-
-class SDeleteFilter(BaseModel):
-    key: str
-    value: Any
-    model_config = ConfigDict(from_attributes=True)
